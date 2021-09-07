47aaf3d05a85355c53361c39a6eb25fea82e5e93
beaniejoy <lhb1025@gmail.com>
2021-09-08 03:06:30 +0900

    [#1] 프로젝트 구성

---
+++ src/main/java/io/beaniejoy/dongecafe/DongneCafeSirenOrderApplication.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.dongecafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongneCafeSirenOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongneCafeSirenOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/beaniejoy/dongecafe/DongneCafeSirenOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.dongecafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongneCafeSirenOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
