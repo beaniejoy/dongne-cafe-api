42c8b3df142c645d24f3cd31fe074eaea800d55b
Hanbin Lee <lhb1025@gmail.com>
2023-03-03 01:28:34 +0900

    [#33] test: migration > test > build 테스트

--- script/db_migration.sh
+++ script/db_migration.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "#### Flyway Config File: $1"
-
+echo "================ 1. Flyway Info ================"
 ./gradlew :db:flywayInfo -Dconfig=$1
 
+echo "================ 2. Flyway Validate ============"
 ./gradlew :db:flywayValidate -Dconfig=$1
+
+echo "================ 3. Flyway Validate ============"
+./gradlew :db:flywayMigrate -Dconfig=$1
+
