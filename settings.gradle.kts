37c590992637beb09b7b0539cdf4a2cc40841d11
beaniejoy <lhb1025@gmail.com>
2023-06-03 02:14:06 +0900

    [#55] modify: app 모듈 생성 및 기존 모듈 이동
    - dongne-service-api, dongne-account-api module > app/service-api, app/account-api 모듈로 이동

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
 rootProject.name = "dongne-cafe-api"
 include("domain")
-include("dongne-service-api")
-include("dongne-account-api")
-include("domain")
+include("app:account-api")
+include("app:service-api")
